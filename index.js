61cf98ab6eeb0a398ff0f2d2514b114043b5767a
Eli Craig <elidcraig@gmail.com>
2022-04-28 17:25:25 -0700

    commit tree d1775a5327a1248ab4d034cd0939490357949bed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+//Removing the element with ID 'main'
+const main = document.getElementById('main')
+main.remove()
+
+//Storing h1#victory in variable newHeader
+const newHeader = document.createElement('h1')
+document.body.append(newHeader)
+newHeader.id = 'victory'
+
+//Editing the innerText of h1#victory to read "YOUR-NAME is the champion"
+newHeader.innerText = '"YOUR-NAME" is the champion'
